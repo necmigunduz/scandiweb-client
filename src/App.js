cd8de9cb74cadb434436af37eb76721083442edb
necmigunduz <necmigunduz@gmail.com>
2022-01-05 12:29:06 +0300

    Add fetchData

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+
+const PRODUCTS_QUERY = `{
+	categories {
+    products {
+      id
+      name
+      brand
+      description
+      inStock
+      gallery
+      prices {
+        amount
+        currency {
+          label
+          symbol
+        }
+      }
+      attributes {
+        id
+        name
+        type
+        items {
+          id
+          value
+          displayValue
+        }
+      }
+    }
+  }
+}`
 
 function App() {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:4000/', {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({ query: PRODUCTS_QUERY })
+    })
+      .then(response => response.json())
+      .then(data => setProducts(data.data.categories[0].products))
+      .catch(error => console.log(error.message))
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Products</h1>
+      <ul>
+        {products}
+      </ul>
     </div>
   );
 }
