78260af49c1a3f7a5215ebef694f71bb5a9afba4
necmigunduz <necmigunduz@gmail.com>
2022-01-05 19:37:06 +0300

    Add product list to app.js

--- src/App.js
+++ src/App.js
@@ -43,12 +43,19 @@ function App() {
       .then(data => setProducts(data.data.categories[0].products))
       .catch(error => console.log(error.message))
   }, [])
-  
+  console.log(products)
   return (
     <div className="App">
-      <h1>Products</h1>
+      <h2>Products</h2>
       <ul>
-        {products}
+        {products.map((product) => {
+          return (
+            <li key={product.id} className='product-li'>
+              <div>{product.name}</div>
+              <img src={product.gallery[0]} alt='Product Photo' width={"200px"}/>
+            </li>
+          )
+        })}
       </ul>
     </div>
   );
