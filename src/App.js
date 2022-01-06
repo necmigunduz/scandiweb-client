6a0c008ebf6d23dfd1acd734e9867cc4691cf220
necmigunduz <necmigunduz@gmail.com>
2022-01-06 17:16:34 +0300

    Transform function App to class App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect, useState } from 'react'
+import React, { Component } from 'react'
 
 const PRODUCTS_QUERY = `{
 	categories {
@@ -31,34 +31,45 @@ const PRODUCTS_QUERY = `{
   }
 }`
 
-function App() {
-  const [products, setProducts] = useState([])
-  useEffect(() => {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: []
+    }
+  }
+
+  componentDidMount() {
     fetch('http://localhost:4000/', {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({ query: PRODUCTS_QUERY })
     })
       .then(response => response.json())
-      .then(data => setProducts(data.data.categories[0].products))
+      .then(data => {
+        this.setState({ products: data.data.categories[0].products })
+      })
       .catch(error => console.log(error.message))
-  }, [])
-  console.log(products)
-  return (
-    <div className="App">
-      <h2>Products</h2>
-      <ul>
-        {products.map((product) => {
-          return (
-            <li key={product.id} className='product-li'>
-              <div>{product.name}</div>
-              <img src={product.gallery[0]} alt='Product Photo' width={"200px"}/>
-            </li>
-          )
-        })}
-      </ul>
-    </div>
-  );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Products</h2>
+        <ul>
+          {this.state.products.map((product) => {
+            return (
+              <li key={product.id} className='product-li'>
+                <div>{product.name}</div>
+                <img src={product.gallery[0]} alt='products' width={"200px"}/>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    );
+  }
+
 }
 
 export default App;
