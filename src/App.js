52c6b5926264a4362d8f764bc94d022980801a67
necmigunduz <necmigunduz@gmail.com>
2022-01-08 10:42:09 +0300

    Fix App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import './App.css';
 import React, { Component } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ProductList from './Components/productList';
 import TopNav from '../src/Components/topNav'
 
 class App extends Component {
   render() {
-    return (
-      <>
-        <BrowserRouter>
-          <div className="App">
-            <TopNav />
-            <Switch>
-              <ProductList />
-            </Switch>
-          </div>
-        </BrowserRouter>
-      </>
+    return (      
+      <div className="App">
+        <TopNav />
+        <ProductList />
+      </div>
     );
   }
 }
