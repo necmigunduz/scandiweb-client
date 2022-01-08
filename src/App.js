c188033941ad505b04ec43935d0da4cc8d10afce
necmigunduz <necmigunduz@gmail.com>
2022-01-08 10:13:13 +0300

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
 import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ProductList from './Components/productList';
 import TopNav from '../src/Components/topNav'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <TopNav />
-        <ProductList />
-      </div>
+      <>
+        <BrowserRouter>
+          <div className="App">
+            <TopNav />
+            <Switch>
+              <ProductList />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </>
     );
   }
-
 }
 
 export default App;
